090a1d715000663e9875a200526efb9f9d160c88
kuus <kunderikuus@gmail.com>
2015-10-08 15:59:34 +0100

    new info in gulp config

--- gulp/config.js
+++ gulp/config.js
@@ -12,7 +12,7 @@ module.exports = {
     ' * <%= pkg.config.namePretty %> v<%= pkg.version %> (<%= pkg.homepage %>)',
     ' * <%= pkg.description %>',
     ' * Copyright (c) <%= pkg.config.startYear %><% if (new Date().getFullYear() > pkg.config.startYear) { %>-<%= new Date().getFullYear() %><% } %> <%= pkg.author.name %> <<%= pkg.author.email %>> (<%= pkg.author.url %>)',
-    ' * <%= pkg.license %> <% var d = new Date(); print(d.getDate() + '-' + d.getMonth() + '-' + d.getFullYear()) %>',
+    ' * <%= pkg.license %> <% var d = new Date(); print(d.getDate() + "-" + d.getMonth() + "-" + d.getFullYear()) %>',
     ' */'
   ].join('\n'),
   argv: require('minimist')(process.argv.slice(2)),
