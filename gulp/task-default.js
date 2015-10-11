1278ae910406abd0a6cdb9eaf39dae498f722963
kuus <kunderikuus@gmail.com>
2015-10-11 16:29:12 +0100

    try another all method

--- gulp/task-default.js
+++ gulp/task-default.js
@@ -1,17 +1,6 @@
-/* global gulp, $ */
+/* global gulp */
 /* jshint node: true */
 'use strict';
 
 // @access public
 gulp.task('default', ['build', 'watch']);
-
-gulp.task('dist', $.shell.task(['gulp build --dist']));
-// @access public
-gulp.task('all', $.sequence(
-  'release-clean',
-  'build',
-  'release-prepare',
-  'release-lang',
-  'deploy',
-  'zip'
-));
