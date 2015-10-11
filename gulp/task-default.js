fb4bd96e86493f19cb183e837fb52784fda8baa8
kuus <kunderikuus@gmail.com>
2015-10-11 16:08:06 +0100

    try another all method

--- gulp/task-default.js
+++ gulp/task-default.js
@@ -6,11 +6,20 @@
 gulp.task('default', ['build', 'watch']);
 
 // @access public
-gulp.task('all',  $.shell.task([
-  'gulp release-clean',
-  'gulp build --dist',
-  'gulp release-prepare',
-  'gulp release-lang',
-  'gulp deploy',
-  'gulp zip'
-]));
+// gulp.task('all',  $.shell.task([
+//   'gulp release-clean',
+//   'gulp build --dist',
+//   'gulp release-prepare',
+//   'gulp release-lang',
+//   'gulp deploy',
+//   'gulp zip'
+// ]));
+
+gulp.task('all', $.sequence.task(['_all-pre', '_all-post']);
+gulp.task('_all-pre', $.shell.task(['gulp release-clean', 'gulp build --dist']);
+gulp.task('_all-post', ['_all-pre'], $.sequence.task([
+  'release-prepare',
+  'release-lang',
+  'deploy',
+  'zip'
+]);
