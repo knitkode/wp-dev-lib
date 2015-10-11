95b4e51263937e46dd0b992cdcff13316c2d5bb5
kuus <kunderikuus@gmail.com>
2015-10-11 16:57:47 +0100

    change zip dest path, giitgnored

--- gulp/task-zip.js
+++ gulp/task-zip.js
@@ -11,12 +11,12 @@ gulp.task('zip', ['_zip-archive', '_zip-last']);
 gulp.task('_zip-archive', ['_deploy-copy_basic_files'], function () {
   return gulp.src('./build/**/*')
     .pipe($.zip(pkg.name + '--' + pkg.version + '.zip'))
-    .pipe(gulp.dest('../roots/releases/archive'));
+    .pipe(gulp.dest('../roots/.publish/releases/archive'));
 });
 
 // @access private
 gulp.task('_zip-last', function () {
   return gulp.src('./build/**/*')
     .pipe($.zip(pkg.name + '.zip'))
-    .pipe(gulp.dest('../roots/releases'));
+    .pipe(gulp.dest('../roots/.publish/releases'));
 });
