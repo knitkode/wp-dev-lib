51962e5138a24b8c64ebd52eae3d905c2ab8b04c
kuus <kunderikuus@gmail.com>
2015-10-11 16:29:28 +0100

    add all gulp task

---
+++ gulp/task-all.js
@@ -0,0 +1,20 @@
+/* global gulp, $ */
+/* jshint node: true */
+'use strict';
+
+// @access public
+gulp.task('all', $.sequence(
+  '_all-set-dist',
+  'release-clean',
+  'build',
+  'release-prepare',
+  'release-lang',
+  'deploy',
+  'zip'
+));
+
+// @access private
+gulp.task('_all-set-dist', function (cb) {
+  global.CONFIG.isDist = true;
+  cb();
+});
