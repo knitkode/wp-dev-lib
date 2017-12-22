512d2a4a1cf9a3191a0213bcbe618f5ae8fd9bca
kuus <kunderikuus@gmail.com>
2017-12-22 20:00:15 +0100

    wip

--- gulp/task-zip.js
+++ gulp/task-zip.js
@@ -3,20 +3,27 @@
 'use strict';
 
 var pkg = require('../../../package.json');
+var del = require('del');
 
 // @access public
-gulp.task('zip', ['_zip-archive', '_zip-last']);
+gulp.task('zip', $.sequence('_zip-preclean', ['_zip-archive', '_zip-last']));
+
+
+// @access private
+gulp.task('_zip-preclean', function () {
+ del.sync(PATHS.build.root + '/*.md');
+});
 
 // @access private
 gulp.task('_zip-archive', function () {
   return gulp.src('./build/**/*')
-    .pipe($.zip(pkg.name + '--' + pkg.version + '.zip'))
+    .pipe($.zip(pkg.config.slug + '--' + pkg.version + '.zip'))
     .pipe(gulp.dest('../roots/.publish/releases/archive'));
 });
 
 // @access private
 gulp.task('_zip-last', function () {
   return gulp.src('./build/**/*')
-    .pipe($.zip(pkg.name + '.zip'))
+    .pipe($.zip(pkg.config.slug + '.zip'))
     .pipe(gulp.dest('../roots/.publish/releases'));
 });
