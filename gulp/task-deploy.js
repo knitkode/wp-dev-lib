e362af03d6e9cb2a2b12b6eb98a8d5c3eaf64361
kuus <kunderikuus@gmail.com>
2017-12-28 16:57:10 +0100

    extendable deploy task

--- gulp/task-deploy.js
+++ gulp/task-deploy.js
@@ -2,13 +2,17 @@
 /* jshint node: true */
 'use strict';
 
+var PATHS = global.PATHS;
+var pkg = require('../../../package.json');
+const ghPages = require('gh-pages');
+const extend = require('extend');
+
 // @access public
 gulp.task('deploy', ['_deploy-github']);
 
 // @access private
 gulp.task('_deploy-github', function () {
-  return require('gh-pages').publish('build', {
-    branch: 'production',
+  return ghPages.publish(PATHS.build.root, extend({
     remote: 'github'
-  });
+  }, pkg.config.deployGithub || {}), callback);
 });
